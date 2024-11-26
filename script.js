d2c7e8ba641ab2581747d6c2151ec2c4b17674aa
StrigZ <mirgasan200@gmail.com>
2024-11-26 06:27:15 +0300

    feat: add ui and refactor code

--- script.js
+++ script.js
@@ -1,58 +1,114 @@
-const returnOptions = ["rock", "paper", "scissors"];
-
+const options = document.querySelectorAll(".options button");
+const humanScoreElement = document.querySelector(".score .human");
+const computerScoreElement = document.querySelector(".score .computer");
+const computerChoiceElement = document.querySelector(".computer");
+const roundResultElement = document.querySelector(".score .result");
+let computerScore = 0;
+let humanScore = 0;
+let computerChoice = null;
+let humanChoice = null;
+let winner = null;
 const getComputerChoice = () => {
+  const returnOptions = ["Rock", "Paper", "Scissors"];
+
   const randomIndex = Math.floor(Math.random() * 3);
-  return returnOptions[randomIndex];
+  computerChoice = returnOptions[randomIndex];
 };
 
-const getHumanChoice = () => {
-  let humanChoice = prompt("What is your choice?").toLowerCase().trim();
-  while (!returnOptions.includes(humanChoice)) {
-    humanChoice = prompt("Available options: 'rock', 'paper', 'scissors'");
+const calcWinner = () => {
+  computerChoice = computerChoice;
+
+  switch (humanChoice) {
+    case computerChoice:
+      winner = "tie";
+      break;
+    case "Rock":
+      computerChoice === "Paper" ? (winner = "computer") : (winner = "human");
+      break;
+    case "Scissors":
+      computerChoice === "Rock" ? (winner = "computer") : (winner = "human");
+      break;
+    case "Paper":
+      computerChoice === "Scissors"
+        ? (winner = "computer")
+        : (winner = "human");
+      break;
+
+    default:
+      break;
   }
-  return humanChoice;
+  return winner;
+};
+const updateUI = () => {
+  humanScoreElement.textContent = humanScore;
+  computerScoreElement.textContent = computerScore;
+  computerChoiceElement.textContent = computerChoice ?? "Choose you option";
+
+  let roundResultText = "";
+  switch (winner) {
+    case "human":
+      roundResultText = "WIN";
+      break;
+    case "computer":
+      roundResultText = "LOSE";
+      break;
+    case "tie":
+      roundResultText = "TIE";
+      break;
+    case null:
+      break;
+    default:
+      break;
+  }
+
+  roundResultElement.textContent = roundResultText;
+  roundResultElement.className = `result ${roundResultText.toLowerCase()}`;
 };
 
-const playGame = () => {
-  let computerScore = 0;
-  let humanScore = 0;
+const updateScore = () => {
+  winner === "human" ? humanScore++ : computerScore++;
+};
 
-  const playRound = (computerChoice, humanChoice) => {
-    switch (humanChoice) {
-      case computerChoice:
-        console.log("TIE");
-        console.log(`Computer: ${computerChoice}, You: ${humanChoice}`);
-        break;
-      case "rock":
-        console.log(computerChoice === "paper" ? "You Lose." : "You Won.");
-        console.log(`Computer: ${computerChoice}, You: ${humanChoice}`);
-        computerChoice === "paper" ? computerScore++ : humanScore++;
-        break;
-      case "scissors":
-        console.log(computerChoice === "rock" ? "You Lose." : "You Won.");
-        console.log(`Computer: ${computerChoice}, You: ${humanChoice}`);
-        computerChoice === "rock" ? computerScore++ : humanScore++;
-        break;
-      case "paper":
-        console.log(computerChoice === "scissors" ? "You Lose." : "You Won.");
-        console.log(`Computer: ${computerChoice}, You: ${humanChoice}`);
-        computerChoice === "scissors" ? computerScore++ : humanScore++;
-        break;
+const playRound = () => {
+  getComputerChoice();
+  calcWinner();
+  updateScore();
+  updateUI();
+};
 
-      default:
-        break;
+const resetButtonHighlight = () =>
+  options.forEach((btn) => btn.classList.remove("clicked"));
+
+const resetGame = () => {
+  computerScore = 0;
+  humanScore = 0;
+  computerChoice = null;
+  humanChoice = null;
+  winner = null;
+  updateUI();
+
+  resetButtonHighlight();
+};
+const checkWinner = () => {
+  if (humanScore === 5) {
+    alert("You won!");
+    resetGame();
+  } else if (computerScore === 5) {
+    alert("You lost!");
+    resetGame();
+  }
+};
+
+options.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    {
+      // Highlight clicked button
+      resetButtonHighlight();
+      button.classList.add("clicked");
+
+      humanChoice = e.target.value;
+      playRound();
+      checkWinner();
     }
-  };
-
-  for (let i = 0; i < 5; i++) {
-    playRound(getComputerChoice(), getHumanChoice());
-  }
-
-  console.log(
-    humanScore > computerScore ? "YOU WON THE GAME!" : "YOU LOST THE GAME"
-  );
-  console.log(`Final score:
-    Computer: ${computerScore}
-    You: ${humanScore}
-    `);
-};
+  });
+});
